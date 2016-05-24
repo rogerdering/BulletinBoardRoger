57592bfb937cb47f8af8be583c61a41389393a2f
Roger Dering <rogerdering@hotmail.com>
2016-05-24 17:19:59 +0200

    just intermittend commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set ( 'views', '.' )
 app.set( 'view engine', 'jade' )
 
 app.get ( '/', function ( request, response ) {
-	var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
+	var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost:5432/bulletinboard';
 	pg.connect(connectionString, function (err, client, done) {
 		if (err) {
 			if (client) {
@@ -43,7 +43,7 @@ app.get ( '/post', function ( request, response ) {
 } )
 
 app.post ( '/post', function ( request, response ) {
-	var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
+	var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost:5432/bulletinboard';
 	pg.connect(connectionString, function (err, client, done) {
 		if (err) {
 			if (client) {
@@ -52,7 +52,7 @@ app.post ( '/post', function ( request, response ) {
 			return;
 		}
 
-		client.query('insert into messages values ($1, $2)', [request.body], function (err) {
+		client.query('insert into messages (title, body) values ($1, $2)', [request.body], function (err) {
 			if(err) {
 				throw err;
 			}
